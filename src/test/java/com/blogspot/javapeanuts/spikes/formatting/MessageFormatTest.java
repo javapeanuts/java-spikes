b4013facef8823fe83ada5775aa9dfc36a55df56
Pietro Martinelli <amicofragile@gmail.com>
2013-04-02 10:36:02 +0200

    SPIKE TEST for MessageFormat.format with repeated positional parameter

--- src/test/java/com/blogspot/javapeanuts/spikes/formatting/MessageFormatTest.java
+++ src/test/java/com/blogspot/javapeanuts/spikes/formatting/MessageFormatTest.java
@@ -11,4 +11,9 @@ public class MessageFormatTest {
 	public void formatStringUsingPositionalParameters() throws Exception {
 		assertEquals("From |xx| to |yy|", MessageFormat.format("From |{0}| to |{1}|", "xx", "yy"));
 	}
+	
+	@Test
+	public void formatStringUsingRepeatedPositionalParameters() throws Exception {
+		assertEquals("For each x, x == x", MessageFormat.format("For each {0}, {0} == {0}", "x"));
+	}
 }
