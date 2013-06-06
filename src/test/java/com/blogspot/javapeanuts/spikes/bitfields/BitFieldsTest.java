b4d1858b3ef811f8092c8c4062805250b135e861
Pietro Martinelli <amicofragile@gmail.com>
2013-06-06 09:08:03 +0200

    SPIKE Getting bit field using bitwise and

--- src/test/java/com/blogspot/javapeanuts/spikes/bitfields/BitFieldsTest.java
+++ src/test/java/com/blogspot/javapeanuts/spikes/bitfields/BitFieldsTest.java
@@ -12,4 +12,12 @@ public class BitFieldsTest {
 		field = field | 4;
 		assertEquals(12, field);
 	}
+	
+	@Test
+	public void getBitFieldUsingAnd() throws Exception {
+		int field = 20;
+		assertTrue((field & 16) != 0);
+		assertTrue((field & 4) != 0);
+		assertTrue((field & 8) == 0);
+	}
 }
